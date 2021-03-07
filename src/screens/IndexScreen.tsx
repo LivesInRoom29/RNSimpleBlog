984e9975b993847d49117b7f0f87a07880bc5724
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2021-03-07 16:12:16 -0500

    Create basic IndexScreen components

---
+++ src/screens/IndexScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+
+import BlogContext from '../context/BlogContext';
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Index Screen</Text>
+    </View>
+  );
+};
+
+const styls = StyleSheet.create({});
+
+export default IndexScreen;
